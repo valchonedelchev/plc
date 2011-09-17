e75a9aa936fe7b8f7f7ca62ed32d55679a05ac11
Vulcho Nedelchev <weby@weby-M51Vr>
2011-09-17 18:32:07 +0300

    ~ init file

---
+++ trunk/lib/AController.pm
@@ -0,0 +1,23 @@
+package AController;
+
+use strict;
+use warnings;
+
+sub new
+{
+    my $class = shift;
+    my $this  = {@_};
+    bless $this, $class;
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+=cut
